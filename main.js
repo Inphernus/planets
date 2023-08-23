93714ddb7bd87b10a41fec8d424d2bd45bcd37b2
Frane Lukin <frane@Franes-MacBook-Pro.local>
2023-08-23 06:36:41 +0200

    Planet images loaded on button click

--- main.js
+++ main.js
@@ -7,16 +7,19 @@ const getContent = (button, planetName) => {
       return {
         content: planets[planetIndex].overview.content,
         source: planets[planetIndex].overview.source,
+        image: planets[planetIndex].images.planet
       };
     case "02 Internal Structure":
       return {
         content: planets[planetIndex].structure.content,
         source: planets[planetIndex].structure.source,
+        image: planets[planetIndex].images.internal
       };
     case "03 Surface Geology":
       return {
         content: planets[planetIndex].geology.content,
         source: planets[planetIndex].geology.source,
+        image: planets[planetIndex].images.geology
       };
     default:
       return Error("Case does not exist");
@@ -25,13 +28,15 @@ const getContent = (button, planetName) => {
 const buttons = document.querySelectorAll("button");
 const description = document.querySelector(".description p");
 const source = document.querySelector(".source a");
+const img = document.querySelector(".planet-image img");
 for (const button of buttons) {
   button.addEventListener("click", function () {
     const planetName = document.title
-    buttons.forEach((button) => button.classList.remove(`${planetName.toLowerCase()}-active`));
-    this.classList.add(`${planetName.toLowerCase()}-active`);
-    const { content, source: src } = getContent(this, planetName);
+    buttons.forEach((button) => button.classList.remove('active'));
+    this.classList.add('active');
+    const { content, source: src, image } = getContent(this, planetName);
     description.textContent = content;
     source.href = src;
+    img.src = `.${image}`
   });
 }
