1862b796c05400dce1db021965fdfe1cff877382
Frane Lukin <frane@Franes-MacBook-Pro.local>
2023-08-24 08:21:55 +0200

    Change how we handle geology image

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 import planets from "./shared/data.json";
 const getContent = (button, planetName) => {
   const buttonText = button.textContent.trim();
-  const planetIndex = planets.findIndex((planet) => (planet.name === planetName));
+  const planetIndex = planets.findIndex((planet) => planet.name === planetName);
   switch (buttonText) {
     case "01 Overview":
       return {
         content: planets[planetIndex].overview.content,
         source: planets[planetIndex].overview.source,
-        image: planets[planetIndex].images.planet
+        image: planets[planetIndex].images.planet,
       };
     case "02 Internal Structure":
       return {
         content: planets[planetIndex].structure.content,
         source: planets[planetIndex].structure.source,
-        image: planets[planetIndex].images.internal
+        image: planets[planetIndex].images.internal,
       };
     case "03 Surface Geology":
       return {
         content: planets[planetIndex].geology.content,
         source: planets[planetIndex].geology.source,
-        image: planets[planetIndex].images.geology
+        image: planets[planetIndex].images.planet,
+        geology: planets[planetIndex].images.geology,
       };
     default:
       return Error("Case does not exist");
@@ -31,12 +32,27 @@ const source = document.querySelector(".source a");
 const img = document.querySelector(".planet-image img");
 for (const button of buttons) {
   button.addEventListener("click", function () {
-    const planetName = document.title
-    buttons.forEach((button) => button.classList.remove('active'));
-    this.classList.add('active');
-    const { content, source: src, image } = getContent(this, planetName);
+    const planetName = document.title;
+    buttons.forEach((button) => button.classList.remove("active"));
+    this.classList.add("active");
+    const {
+      content,
+      source: src,
+      image,
+      geology,
+    } = getContent(this, planetName);
     description.textContent = content;
     source.href = src;
-    img.src = `.${image}`
+    img.src = `.${image}`;
+    const geo = document.querySelector(".geology");
+    if (geology) {
+        geo?.remove();
+        const img = document.createElement("img");
+        img.src = `.${geology}`;
+        img.classList.add("geology");
+        document.querySelector('.planet-image').appendChild(img);
+      } else {
+        geo?.remove();
+      }
   });
 }
